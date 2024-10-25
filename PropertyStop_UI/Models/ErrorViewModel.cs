961c629d31d8aebe97c36259241b8b52e348ac84
M.Demir <M.Demir@DEMIR11>
2024-10-25 17:52:40 +0300

    Add project files.

---
+++ PropertyStop_UI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PropertyStop_UI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
