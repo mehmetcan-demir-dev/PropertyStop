2ba7df88604d63aefb0eaaf586af24da95025e3b
mehmetcan-demir-dev <mehmetcandemir11@outlook.com.tr>
2024-10-27 19:55:40 +0300

    Ders 16 Sonu

---
+++ PropertyStop_UI/Dtos/WhoWeAreDtos/ResultServiceDto.cs
@@ -0,0 +1,10 @@
+﻿namespace PropertyStop_UI.Dtos.WhoWeAreDtos
+{
+    public class ResultServiceDto
+    {
+        public int ServiceID{ get; set; }
+        public string ServiceName{ get; set; }
+
+        public bool ServiceStatus { get; set; }
+    }
+}
