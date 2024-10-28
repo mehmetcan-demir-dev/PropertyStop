378bafcf718235158659b88c2acb9f1456c8e5f2
mehmetcan-demir-dev <mehmetcandemir11@outlook.com.tr>
2024-10-29 00:04:33 +0300

    Ders 23 sonu, ders 22 sorunu halledildi, KingIsBack

---
+++ PropertyStop_UI/ViewComponents/AdminLayout/_AdminLayoutSideBarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PropertyStop_UI.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutSideBarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        { 
+            return View(); 
+        }
+    }
+}
---
+++ PropertyStop_UI/ViewComponents/AdminLayout/_AdminLayoutSpinnerComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PropertyStop_UI.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutSpinnerComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
