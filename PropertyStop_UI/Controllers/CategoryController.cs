1f91ffbd5cfbfee1ca050223b457854d4e7ce4c1
mehmetcan-demir-dev <mehmetcandemir11@outlook.com.tr>
2024-10-29 00:58:28 +0300

    ders 25 sonu

--- PropertyStop_UI/Controllers/CategoryController.cs
+++ PropertyStop_UI/Controllers/CategoryController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using PropertyStop_UI.Dtos.CategoryDtos;
+using System.Text;
 
 namespace PropertyStop_UI.Controllers
 {
@@ -22,6 +23,24 @@ namespace PropertyStop_UI.Controllers
                 var jsonData = await responseMessage.Content.ReadAsStringAsync();
                 var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
                 return View(values);
+            }
+            return View();
+        }
+        [HttpGet]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+            StringContent stringContent = new StringContent(jsonData,Encoding.UTF8,"application/json");
+            var responseMessage = await client.PostAsync("https://localhost:44324/api/Categories", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
             }
             return View();
         }
