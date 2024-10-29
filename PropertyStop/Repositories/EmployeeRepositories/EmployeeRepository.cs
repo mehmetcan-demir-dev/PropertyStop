d15ad8093cd1d7c19b92be88f10cdc80e81a5f8e
mehmetcan-demir-dev <mehmetcandemir11@outlook.com.tr>
2024-10-29 17:31:19 +0300

    ders 28 sonu

---
+++ PropertyStop/Repositories/EmployeeRepositories/EmployeeRepository.cs
@@ -0,0 +1,34 @@
+﻿using PropertyStop.Dtos.EmployeeDtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PropertyStop.Repositories.EmployeeRepositories
+{
+    public class EmployeeRepository : IEmployeeRepository
+    {
+        public void CreateEmployee(CreateEmployeeDto EmployeeDto)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteEmployee(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<ResultEmployeeDto>> GetAllEmployeeAsync()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<GetByIDEmployeeDto> GetEmployee(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateEmployee(UpdateEmployeeDto EmployeeDto)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ PropertyStop/Repositories/EmployeeRepositories/IEmployeeRepository.cs
@@ -0,0 +1,15 @@
+﻿using PropertyStop.Dtos.EmployeeDtos;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace PropertyStop.Repositories.EmployeeRepositories
+{
+    public interface IEmployeeRepository
+    {
+        Task<List<ResultEmployeeDto>> GetAllEmployeeAsync();
+        void CreateEmployee(CreateEmployeeDto EmployeeDto);
+        void DeleteEmployee(int id);
+        void UpdateEmployee(UpdateEmployeeDto EmployeeDto);
+        Task<GetByIDEmployeeDto> GetEmployee(int id);
+    }
+}
