53de7ec256664cc15a1799129402fedb3f9c6dd3
mehmetcan-demir-dev <mehmetcandemir11@outlook.com.tr>
2024-11-17 19:59:35 +0300

    ders 45 sonu

--- PropertyStop_UI/Controllers/ProductController.cs
+++ PropertyStop_UI/Controllers/ProductController.cs
@@ -45,5 +45,25 @@ namespace PropertyStop_UI.Controllers
             ViewBag.v = categoryValues;
             return View();
         }
+        public async Task<IActionResult> ProductDealOfTheDayStatusChangeToFalse(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44324/api/ProductControllers/ProductDealOfTheDayStatusChangeToFalse/"+id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+        public async Task<IActionResult> ProductDealOfTheDayStatusChangeToTrue(int id)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44324/api/ProductControllers/ProductDealOfTheDayStatusChangeToTrue/" + id);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
