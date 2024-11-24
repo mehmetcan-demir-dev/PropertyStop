9b2197c264c248d610dc95f2cfb42a340f202b38
mehmetcan-demir-dev <mehmetcandemir11@outlook.com.tr>
2024-11-24 15:00:20 +0300

    ders 55 sonu

---
+++ PropertyStop/Tools/JwtTokenDefault.cs
@@ -0,0 +1,10 @@
+﻿namespace PropertyStop.Tools
+{
+    public class JwtTokenDefault
+    {
+        public const string ValidAudience = "https://localhost";
+        public const string ValidIssuer = "https://localhost";
+        public const string Key = "PropertyStop..0102030405Asp.NetCore8.0.1+-*/";
+        public const int Expire = 5;
+    }
+}
