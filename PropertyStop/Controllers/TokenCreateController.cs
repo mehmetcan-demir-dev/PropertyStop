1e90a227d8461b52bcad429274963d2118d70371
mehmetcan-demir-dev <mehmetcandemir11@outlook.com.tr>
2024-11-24 17:00:08 +0300

    ders 57 sonu

---
+++ PropertyStop/Controllers/TokenCreateController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PropertyStop.Tools;
+
+namespace PropertyStop.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenCreateController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult CreateToken(GetCheckAppUserViewModel model)
+        {
+            var values = JwtTokenGenerator.GenerateToken(model);
+            return Ok(values);
+        }
+    }
+}
---
+++ PropertyStop_UI/ViewComponents/EstateAgent/_EstateAgentLayoutSideBarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PropertyStop_UI.ViewComponents.EstateAgent
+{
+    public class _EstateAgentLayoutSideBarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
