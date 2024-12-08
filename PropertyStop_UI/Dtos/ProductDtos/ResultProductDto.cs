b23f4a661354e4e729227cc0a1c52908e45f2d22
mehmetcan-demir-dev <mehmetcandemir11@outlook.com.tr>
2024-12-08 20:36:46 +0300

    ders 72 sonu

--- PropertyStop_UI/Dtos/ProductDtos/ResultProductDto.cs
+++ PropertyStop_UI/Dtos/ProductDtos/ResultProductDto.cs
@@ -2,15 +2,15 @@
 {
     public class ResultProductDto
     {
-            public int productID { get; set; }
-            public string title { get; set; }
-            public decimal price { get; set; }
-            public string city { get; set; }
-            public string district { get; set; }
-            public string categoryName { get; set; } 
-            public string coverImage { get; set; } 
-            public string type { get; set; } 
-            public string address { get; set; } 
+            public int ProductID { get; set; }
+            public string Title { get; set; }
+            public decimal Price { get; set; }
+            public string City { get; set; }
+            public string District { get; set; }
+            public string CategoryName { get; set; } 
+            public string CoverImage { get; set; } 
+            public string Type { get; set; } 
+            public string Address { get; set; } 
             public bool DealOfTheDay { get; set; }
     }
 }
