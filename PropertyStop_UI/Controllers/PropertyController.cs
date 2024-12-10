661f717c6288c27fb877f71ee38318b32ec45560
mehmetcan-demir-dev <mehmetcandemir11@outlook.com.tr>
2024-12-10 22:03:49 +0300

    ders 74 sonu

--- PropertyStop_UI/Controllers/PropertyController.cs
+++ PropertyStop_UI/Controllers/PropertyController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
+using PropertyStop_UI.Dtos.ProductDetailDtos;
 using PropertyStop_UI.Dtos.ProductDtos;
 
 namespace PropertyStop_UI.Controllers
@@ -28,6 +29,33 @@ namespace PropertyStop_UI.Controllers
         [HttpGet]
         public async Task<IActionResult> PropertySingle(int id)
         {
+            id = 1;
+
+            //Evin açıklaması, fiyatı vs.
+            var client1 = _httpClientFactory.CreateClient();
+            var responseMessage1 = await client1.GetAsync("https://localhost:44324/api/ProductsControllers/GetProductByProductID?id=" + id);
+            var jsonData1 = await responseMessage1.Content.ReadAsStringAsync();
+            var values1 = JsonConvert.DeserializeObject<ResultProductDto>(jsonData1);
+
+            // Resmin üstündeki eve dair bilgiler
+            var client2 = _httpClientFactory.CreateClient();
+            var responseMessage2 = await client2.GetAsync("https://localhost:44324/api/ProductDetails/GetProductDetailByProductID?id=" + id);
+            var jsonData2 = await responseMessage2.Content.ReadAsStringAsync();
+            var values2 = JsonConvert.DeserializeObject<GetProductDetailByIDDto>(jsonData2);
+
+
+            ViewBag.Title1 = values1.Title.ToString();
+            ViewBag.Price = values1.Price;
+            ViewBag.City = values1.City;
+            ViewBag.District = values1.District;
+            ViewBag.Address = values1.Address;
+            ViewBag.Type = values1.Type;
+            ViewBag.BathCount = values2.bathCount;
+            ViewBag.BedroomCount = values2.bedRoomCount;
+            ViewBag.ProductSize = values2.productSize;
+
+
+
             return View();
         }
     }
---
+++ PropertyStop_UI/Dtos/ProductDetailDtos/GetProductDetailByIDDto.cs
@@ -0,0 +1,16 @@
+﻿namespace PropertyStop_UI.Dtos.ProductDetailDtos
+{
+    public class GetProductDetailByIDDto
+    {
+        public int productDetailID { get; set; }
+        public int productSize { get; set; }
+        public int bedRoomCount { get; set; }
+        public int bathCount { get; set; }
+        public int garageSize { get; set; }
+        public string buildYear { get; set; }
+        public decimal price { get; set; }
+        public string location { get; set; }
+        public string videoUrl { get; set; }
+        public int productID { get; set; }
+    }
+}
