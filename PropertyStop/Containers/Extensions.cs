a920b220ee85d6e0f93cf41ac7e7fe30c3e732e8
mehmetcan-demir-dev <mehmetcandemir11@outlook.com.tr>
2024-12-21 12:57:20 +0300

    ders 92 sonu

---
+++ PropertyStop/Containers/Extensions.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PropertyStop.Models.DapperContext;
+using PropertyStop.Repositories.AppUserRepositories;
+using PropertyStop.Repositories.BottomGridRepositories;
+using PropertyStop.Repositories.CategoryRepository;
+using PropertyStop.Repositories.ContactRepositories;
+using PropertyStop.Repositories.EmployeeRepositories;
+using PropertyStop.Repositories.EstateAgentRepositories.DashboardRepositories.ChartRepositories;
+using PropertyStop.Repositories.EstateAgentRepositories.DashboardRepositories.LastProductsRepositories;
+using PropertyStop.Repositories.EstateAgentRepositories.DashboardRepositories.StatisticsRepositories;
+using PropertyStop.Repositories.MessageRepositories;
+using PropertyStop.Repositories.PopularLocationRepositories;
+using PropertyStop.Repositories.ProductImageRepositories;
+using PropertyStop.Repositories.ProductRepository;
+using PropertyStop.Repositories.PropertyAmenityRepositories;
+using PropertyStop.Repositories.ServiceRepository;
+using PropertyStop.Repositories.StatisticsRepositories;
+using PropertyStop.Repositories.SubFeatureRepositories;
+using PropertyStop.Repositories.TestimonialRepositories;
+using PropertyStop.Repositories.ToDoListRepositories;
+using PropertyStop.Repositories.WhoWeAreRepository;
+
+namespace PropertyStop.Containers
+{
+    public static class Extensions
+    {
+        public static void ContainerDependencies(this IServiceCollection services)
+        {
+            services.AddTransient<Context>();
+            services.AddTransient<ICategoryRepository, CategoryRepository>();
+            services.AddTransient<IProductRepository, ProductRepository>();
+            services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
+            services.AddTransient<IServiceRepository, ServiceRepository>();
+            services.AddTransient<IBottomGridRepository, BottomGridRepository>();
+            services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
+            services.AddTransient<IStatisticsRepository, StatisticsRepository>();
+            services.AddTransient<ITestimonialRepository, TestimonialRepository>();
+            services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+            services.AddTransient<IContactRepository, ContactRepository>();
+            services.AddTransient<IToDoListRepository, ToDoListRepository>();
+            services.AddTransient<IStatisticRepository, StatisticRepository>();
+            services.AddTransient<IChartRepository, ChartRepository>();
+            services.AddTransient<ILast5ProductRepository, Last5ProductRepository>();
+            services.AddTransient<IMessageRepository, MessageRepository>();
+            services.AddTransient<IProductImageRepository, ProductImageRepository>();
+            services.AddTransient<IAppUserRepository, AppUserRepository>();
+            services.AddTransient<IPropertyAmenityRepository, PropertyAmenityRepository>();
+            services.AddTransient<ISubFeatureRepository, SubFeatureRepository>();
+        }
+    }
+}
--- PropertyStop/Program.cs
+++ PropertyStop/Program.cs
@@ -1,52 +1,16 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using PropertyStop.Containers;
 using PropertyStop.Hubs;
-using PropertyStop.Models.DapperContext;
-using PropertyStop.Repositories.AppUserRepositories;
-using PropertyStop.Repositories.BottomGridRepositories;
-using PropertyStop.Repositories.CategoryRepository;
-using PropertyStop.Repositories.ContactRepositories;
-using PropertyStop.Repositories.EmployeeRepositories;
-using PropertyStop.Repositories.EstateAgentRepositories.DashboardRepositories.ChartRepositories;
-using PropertyStop.Repositories.EstateAgentRepositories.DashboardRepositories.LastProductsRepositories;
-using PropertyStop.Repositories.EstateAgentRepositories.DashboardRepositories.StatisticsRepositories;
-using PropertyStop.Repositories.MessageRepositories;
-using PropertyStop.Repositories.PopularLocationRepositories;
-using PropertyStop.Repositories.ProductImageRepositories;
-using PropertyStop.Repositories.ProductRepository;
-using PropertyStop.Repositories.PropertyAmenityRepositories;
-using PropertyStop.Repositories.ServiceRepository;
-using PropertyStop.Repositories.StatisticsRepositories;
-using PropertyStop.Repositories.SubFeatureRepositories;
-using PropertyStop.Repositories.TestimonialRepositories;
-using PropertyStop.Repositories.ToDoListRepositories;
-using PropertyStop.Repositories.WhoWeAreRepository;
-
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddTransient<Context>();
-builder.Services.AddTransient<ICategoryRepository, CategoryRepository>();
-builder.Services.AddTransient<IProductRepository, ProductRepository>();
-builder.Services.AddTransient<IWhoWeAreDetailRepository, WhoWeAreDetailRepository>();
-builder.Services.AddTransient<IServiceRepository, ServiceRepository>();
-builder.Services.AddTransient<IBottomGridRepository, BottomGridRepository>();
-builder.Services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
-builder.Services.AddTransient<IStatisticsRepository, StatisticsRepository>();
-builder.Services.AddTransient<ITestimonialRepository, TestimonialRepository>();
-builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
-builder.Services.AddTransient<IContactRepository, ContactRepository>();
-builder.Services.AddTransient<IToDoListRepository, ToDoListRepository>();
-builder.Services.AddTransient<IStatisticRepository, StatisticRepository>();
-builder.Services.AddTransient<IChartRepository, ChartRepository>();
-builder.Services.AddTransient<ILast5ProductRepository, Last5ProductRepository>();
-builder.Services.AddTransient<IMessageRepository, MessageRepository>();
-builder.Services.AddTransient<IProductImageRepository, ProductImageRepository>();
-builder.Services.AddTransient<IAppUserRepository, AppUserRepository>();
-builder.Services.AddTransient<IPropertyAmenityRepository, PropertyAmenityRepository>();
-builder.Services.AddTransient<ISubFeatureRepository, SubFeatureRepository>();
+
+
+builder.Services.ContainerDependencies();
+
 
 builder.Services.AddCors(opt =>
 {
